febc8ec258e3c943e29bff909c7c63ca7b299266
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-04-19 13:54:42 +0200

    Define the hibernate context for Message.

---
+++ data/src/main/java/nl/tehdreamteam/se42/data/context/impl/MessageHibernateContext.java
@@ -0,0 +1,21 @@
+package nl.tehdreamteam.se42.data.context.impl;
+
+import nl.tehdreamteam.se42.data.dao.MessageDAO;
+import nl.tehdreamteam.se42.data.dao.impl.HibernateMessageDAO;
+import nl.tehdreamteam.se42.domain.Message;
+
+import javax.persistence.EntityManager;
+
+/**
+ * A {@code MessageHibernateContext} defines communication between the database and a {@link MessageDAO}.
+ *
+ * @author Lesley
+ */
+public final class MessageHibernateContext extends HibernateContext<Long, Message, MessageDAO> {
+
+    @Override
+    protected MessageDAO createDao(EntityManager manager) {
+        return new HibernateMessageDAO(manager);
+    }
+
+}
