f27d0fd62abd79745bf44d8f5359807a1b59c33a
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-04-19 13:57:42 +0200

    Added Conversation class.

---
+++ domain/src/main/java/nl/tehdreamteam/se42/domain/Conversation.java
@@ -0,0 +1,87 @@
+package nl.tehdreamteam.se42.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Oscar de Leeuw
+ */
+@Entity
+public class Conversation {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private List<User> participants;
+    private List<Message> messages;
+
+    /**
+     * Creates a new Conversation.
+     */
+    public Conversation() {
+        this(new LinkedList<>(), new LinkedList<>());
+    }
+
+    /**
+     * Creates a new Conversation.
+     *
+     * @param participants The participants of this conversation.
+     */
+    public Conversation(List<User> participants) {
+        this(participants, new LinkedList<>());
+    }
+
+    private Conversation(List<User> participants, List<Message> messages) {
+        this.participants = participants;
+        this.messages = messages;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    /**
+     * Adds an User to this Conversation.
+     *
+     * @param user The User to add to this Conversation.
+     */
+    public void addUser(User user) {
+        participants.add(user);
+        user.addConversation(this);
+    }
+
+    /**
+     * Removes an User from this Conversation.
+     *
+     * @param user The User to remove from this Conversation.
+     */
+    public void removeUser(User user) {
+        participants.remove(user);
+        user.removeConversation(this);
+    }
+
+    /**
+     * Adds a Message to the Conversation.
+     *
+     * @param message The Message to add to this Conversation.
+     */
+    public void addMessage(Message message) {
+        messages.add(message);
+    }
+
+    /**
+     * Removes a Message from this Conversation.
+     *
+     * @param message The Message to remove from this Conversation.
+     */
+    public void removeMessage(Message message) {
+        messages.remove(message);
+    }
+}
