150916a8d7f4a30c15082c2e0ead300e375849fe
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-04-19 13:57:26 +0200

    Added LoginCredentials class.

---
+++ domain/src/main/java/nl/tehdreamteam/se42/domain/LoginCredentials.java
@@ -0,0 +1,38 @@
+package nl.tehdreamteam.se42.domain;
+
+import javax.persistence.Embeddable;
+
+/**
+ * LoginCredentials are the credentials of an User.
+ *
+ * @author Oscar de Leeuw
+ */
+@Embeddable
+public class LoginCredentials {
+
+    private String username;
+    private String password;
+
+    private LoginCredentials() {
+
+    }
+
+    /**
+     * Creates a new LoginCredentials.
+     *
+     * @param username The username of the user.
+     * @param password The password of the user.
+     */
+    public LoginCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
