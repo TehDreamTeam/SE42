5b2855c8928d85803e6cf3e7323b786aca4e78d9
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-05-10 13:00:40 +0200

    Removed TODO and fixed documentation.

--- web/src/main/java/nl/tehdreamteam/se42/web/controller/UserController.java
+++ web/src/main/java/nl/tehdreamteam/se42/web/controller/UserController.java
@@ -4,15 +4,13 @@ import nl.tehdreamteam.se42.domain.User;
 
 public interface UserController {
 
-    // TODO, should not be a String, probably a dedicated Token class
-
     /**
      * Logs a {@link User} in, given that the specified {@code username} and {@code password} are correct, and returns
-     * a {@code token} that temporarily identifies the logged-in {@code User}.
+     * a {@code Token value} that temporarily identifies the logged-in {@code User}.
      *
      * @param username The {@code username} of the {@code User} to login.
      * @param password The {@code password} of the {@code User} to login.
-     * @return A {@code token} that temporarily identifies the logged-in {@code User}.
+     * @return A {@code Token value} that temporarily identifies the logged-in {@code User}.
      */
     String login(String username, String password);
 
