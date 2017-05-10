7ed97f777ced6ccdd54d50628dc2f580548b1d34
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-05-10 10:59:47 +0200

    Add documentation for UserController and move to more representative package.

--- web/src/main/java/nl/tehdreamteam/se42/web/controller/UserController.java
+++ web/src/main/java/nl/tehdreamteam/se42/web/controller/UserController.java
@@ -1,10 +1,12 @@
-package nl.tehdreamteam.se42.web.user;
+package nl.tehdreamteam.se42.web.controller;
 
 import nl.tehdreamteam.se42.domain.User;
+import nl.tehdreamteam.se42.web.Service;
 
+/**
+ * Defines functions that specific {@link Service Services} can use to serve content.
+ */
 public interface UserController {
-
-    // TODO, should not be a String, probably a dedicated Token class
 
     /**
      * Logs a {@link User} in, given that the specified {@code username} and {@code password} are correct, and returns
@@ -14,6 +16,6 @@ public interface UserController {
      * @param password The {@code password} of the {@code User} to login.
      * @return A {@code token} that temporarily identifies the logged-in {@code User}.
      */
-    String login(String username, String password);
+    String login(String username, String password); // TODO, should not be a String, probably a dedicated Token class
 
 }
