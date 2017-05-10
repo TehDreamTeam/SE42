a13c0812e7235d8ed73e50ef4b4ae014e93c108f
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-05-10 13:49:24 +0200

    Added getConversation method to UserController.

--- web/src/main/java/nl/tehdreamteam/se42/web/controller/UserController.java
+++ web/src/main/java/nl/tehdreamteam/se42/web/controller/UserController.java
@@ -3,6 +3,8 @@ package nl.tehdreamteam.se42.web.controller;
 import nl.tehdreamteam.se42.domain.User;
 import nl.tehdreamteam.se42.web.Service;
 
+import java.util.List;
+
 /**
  * Defines functions that specific {@link Service Services} can use to serve content.
  */
@@ -16,6 +18,14 @@ public interface UserController {
      * @param password The {@code password} of the {@code User} to login.
      * @return A {@code Token value} that temporarily identifies the logged-in {@code User}.
      */
-    String login(String username, String password); // TODO, should not be a String, probably a dedicated Token class
+    String login(String username, String password);
+
+    /**
+     * Gets a List of all the ids of the {@code Conversations} that the user participates in.
+     *
+     * @param token A valid token that is needed to verify a user.
+     * @return A List of all the id's.
+     */
+    List<Long> getConversations(String token);
 
 }
--- web/src/main/java/nl/tehdreamteam/se42/web/soap/user/SoapUserController.java
+++ web/src/main/java/nl/tehdreamteam/se42/web/soap/user/SoapUserController.java
@@ -1,19 +1,39 @@
 package nl.tehdreamteam.se42.web.soap.user;
 
+import nl.tehdreamteam.se42.domain.LoginCredentials;
+import nl.tehdreamteam.se42.domain.User;
 import nl.tehdreamteam.se42.web.controller.UserController;
+import nl.tehdreamteam.se42.web.soap.SoapWebServiceConstants;
+import nl.tehdreamteam.se42.web.token.TokenFactory;
 
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Implementation of {@link UserController} that receives and serves SOAP messages.
  */
-@WebService
+@WebService(targetNamespace = SoapWebServiceConstants.DEFAULT_NAMESPACE + "user")
 public class SoapUserController implements UserController {
+
+    private TokenFactory tokenFactory;
+
+    /**
+     * Creates a new {@code SoapUserController}.
+     */
+    public SoapUserController() {
+        tokenFactory = new TokenFactory();
+    }
 
     @Override
     public String login(@WebParam(name = "username") String username, @WebParam(name = "password") String password) {
-        return username + "-" + password;
+        return tokenFactory.createToken(new User(new LoginCredentials(username, password))).getValue();
+    }
+
+    @Override
+    public List<Long> getConversations(String token) {
+        return Arrays.asList(1L, 2L, 4L);
     }
 
 }
