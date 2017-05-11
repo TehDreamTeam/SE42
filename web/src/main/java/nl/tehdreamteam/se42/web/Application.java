f61a76f5e2180b5b22093c86ede23b1bacf45bbf
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-05-11 14:48:43 +0200

    Refactor Application class to not use statics.

--- web/src/main/java/nl/tehdreamteam/se42/web/Application.java
+++ web/src/main/java/nl/tehdreamteam/se42/web/Application.java
@@ -18,35 +18,49 @@ public final class Application {
 
     private static final Logger logger = LogManager.getLogger(Application.class.getSimpleName());
 
+    private final CommandHandler handler;
+    private final ServiceContainer container;
+
+    private Application() {
+        container = new ServiceContainer();
+        container.addService(new SoapWebService());
+        container.startAll();
+
+        handler = new CommandHandler();
+        handler.register(new StartServicesCommand(container));
+        handler.register(new StopServicesCommand(container));
+        handler.register(new ExitApplicationCommand(container));
+    }
+
+    private void startReadingCLI() {
+        try (Scanner sc = new Scanner(System.in)) {
+            processScannerInput(sc);
+        }
+    }
+
+    private void processScannerInput(Scanner sc) {
+        String line;
+        while ((line = sc.nextLine()) != null) {
+            processReadLine(line);
+        }
+    }
+
+    private void processReadLine(String line) {
+        try {
+            handler.handle(line);
+        } catch (Exception e) {
+            logger.error("Failed to execute command: {}.", e.getMessage());
+        }
+    }
+
     /**
      * Starts all services.
      *
      * @param args The command-line arguments.
      */
     public static void main(String[] args) {
-        ServiceContainer container = new ServiceContainer();
-        container.addService(new SoapWebService());
-        container.startAll();
-
-        CommandHandler handler = new CommandHandler();
-        handler.register(new StartServicesCommand(container));
-        handler.register(new StopServicesCommand(container));
-        handler.register(new ExitApplicationCommand(container));
-
-        startReadingCommandLine(handler);
-    }
-
-    private static void startReadingCommandLine(CommandHandler handler) {
-        try (Scanner sc = new Scanner(System.in)) {
-            String line;
-            while ((line = sc.nextLine()) != null) {
-                try {
-                    handler.handle(line);
-                } catch (Exception e) {
-                    logger.error("Failed to execute command: {}.", e.getMessage());
-                }
-            }
-        }
+        Application application = new Application();
+        application.startReadingCLI();
     }
 
 }
