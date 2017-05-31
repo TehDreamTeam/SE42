cbe93ac7bf4c5a5c9c9973cf71d19c274abaa4a9
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-05-31 14:57:48 +0200

    Added documentation for HashTokenRegistry.

--- web/src/main/java/nl/tehdreamteam/se42/web/token/impl/HashTokenRegistry.java
+++ web/src/main/java/nl/tehdreamteam/se42/web/token/impl/HashTokenRegistry.java
@@ -3,21 +3,39 @@ package nl.tehdreamteam.se42.web.token.impl;
 import nl.tehdreamteam.se42.domain.User;
 import nl.tehdreamteam.se42.web.token.AbstractTokenRegistry;
 import nl.tehdreamteam.se42.web.token.Token;
+import nl.tehdreamteam.se42.web.token.TokenRegistry;
 import nl.tehdreamteam.se42.web.token.generator.TokenGenerator;
+import nl.tehdreamteam.se42.web.token.generator.impl.SimpleTokenGenerator;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Implementation of {@link TokenRegistry} that stores {@link Token Tokens} based on their hash.
+ *
+ * @see HashMap
+ */
 public class HashTokenRegistry extends AbstractTokenRegistry {
 
     private final Map<User, Token> tokens = new HashMap<>();
 
+    /**
+     * Initializes this {@code HashTokenRegistry} using a {@link SimpleTokenGenerator}.
+     *
+     * @see AbstractTokenRegistry#AbstractTokenRegistry()
+     */
     public HashTokenRegistry() {
         super();
     }
 
+    /**
+     * Initializes this {@code HashTokenRegistry} using the given {@link TokenGenerator}.
+     *
+     * @param generator The {@code generator} this {@code HashTokenRegistry} uses to generate {@link Token Tokens}.
+     * @see AbstractTokenRegistry#AbstractTokenRegistry(TokenGenerator)
+     */
     public HashTokenRegistry(TokenGenerator generator) {
         super(generator);
     }
