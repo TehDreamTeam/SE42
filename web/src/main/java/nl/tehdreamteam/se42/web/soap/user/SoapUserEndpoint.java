10c7d04a69fd58d30e469d0569b18595f469a39c
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-06-07 14:30:06 +0200

    Added login method.

--- web/src/main/java/nl/tehdreamteam/se42/web/soap/user/SoapUserEndpoint.java
+++ web/src/main/java/nl/tehdreamteam/se42/web/soap/user/SoapUserEndpoint.java
@@ -1,10 +1,11 @@
 package nl.tehdreamteam.se42.web.soap.user;
 
-import nl.tehdreamteam.se42.domain.LoginCredentials;
-import nl.tehdreamteam.se42.domain.User;
+import nl.tehdreamteam.se42.logic.controller.UserController;
+import nl.tehdreamteam.se42.logic.exception.ServerError;
+import nl.tehdreamteam.se42.logic.user.DefaultUserController;
 import nl.tehdreamteam.se42.web.endpoint.UserEndpoint;
 import nl.tehdreamteam.se42.web.soap.SoapWebServiceConstants;
-import nl.tehdreamteam.se42.web.token.Token;
+import nl.tehdreamteam.se42.web.token.TokenRegistry;
 import nl.tehdreamteam.se42.web.token.TokenRegistryFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,14 +23,25 @@ public class SoapUserEndpoint implements UserEndpoint {
 
     private static final Logger logger = LogManager.getLogger(SoapUserEndpoint.class.getSimpleName());
 
+    private final UserController controller;
+    private final TokenRegistry registry;
+
+    public SoapUserEndpoint(UserController controller, TokenRegistry registry) {
+        this.controller = controller;
+        this.registry = registry;
+    }
+
+    public SoapUserEndpoint() {
+        this(new DefaultUserController(), TokenRegistryFactory.getTokenRegistry());
+    }
+
     @Override
     public String login(@WebParam(name = "username") String username, @WebParam(name = "password") String password) {
         logger.debug("Soap request for login (username='{}', password='{}').", username, password);
 
-        User user = new User(new LoginCredentials(username, password));
-        Token token = TokenRegistryFactory.getTokenRegistry().getOrRegister(user);
-
-        return token.getId();
+        return controller.login(username, password)
+                .match(ServerError::getMessage,
+                        r -> registry.getOrRegister(r).getId());
     }
 
     @Override
@@ -39,6 +51,8 @@ public class SoapUserEndpoint implements UserEndpoint {
 
     @Override
     public List<Long> getConversations(String token) {
+
+
         return Arrays.asList(1L, 2L, 4L);
     }
 
