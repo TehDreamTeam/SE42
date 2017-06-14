91e991d2785ecfef8f664fefe6a778bb48ed9d76
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-06-14 10:35:55 +0200

    Correctly decrypt passwords.

--- logic/src/main/java/nl/tehdreamteam/se42/logic/user/DefaultUserController.java
+++ logic/src/main/java/nl/tehdreamteam/se42/logic/user/DefaultUserController.java
@@ -4,19 +4,25 @@ import com.jnape.palatable.lambda.adt.Either;
 import nl.tehdreamteam.se42.data.user.UserRepository;
 import nl.tehdreamteam.se42.data.user.hibernate.UserHibernateRepository;
 import nl.tehdreamteam.se42.domain.Conversation;
+import nl.tehdreamteam.se42.domain.LoginCredentials;
 import nl.tehdreamteam.se42.domain.User;
 import nl.tehdreamteam.se42.logic.BusinessLogic;
 import nl.tehdreamteam.se42.logic.controller.UserController;
 import nl.tehdreamteam.se42.logic.exception.ServerError;
 import nl.tehdreamteam.se42.logic.exception.impl.LoginExceptionHandler;
+import nl.tehdreamteam.se42.logic.security.symmetric.SymmetricEncryption;
 import nl.tehdreamteam.se42.logic.validator.input.InputValidatorChain;
 import nl.tehdreamteam.se42.logic.validator.input.impl.NonNullInputValidator;
 import nl.tehdreamteam.se42.logic.validator.input.impl.NotEmptyStringInputValidator;
 import nl.tehdreamteam.se42.logic.validator.output.OutputValidatorChain;
 import nl.tehdreamteam.se42.logic.validator.output.impl.PasswordOutputValidator;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.List;
 import java.util.function.Supplier;
+
+import static nl.tehdreamteam.se42.logic.security.SecurityConstants.*;
 
 /**
  * Default implementation of the {@code UserController} interface.
@@ -25,6 +31,9 @@ import java.util.function.Supplier;
  */
 public class DefaultUserController implements UserController {
 
+    private final SymmetricEncryption encryption = new SymmetricEncryption(SYMMETRIC_ENCRYPTION_ALGORITHM,
+            DEFAULT_IV_SPEC,
+            DEFAULT_KEY_SPEC);
     private UserRepository userRepository;
 
     /**
@@ -46,7 +55,6 @@ public class DefaultUserController implements UserController {
 
     @Override
     public Either<ServerError, User> login(String username, String password) {
-
         InputValidatorChain inputValidation = new InputValidatorChain();
         inputValidation.add(new NonNullInputValidator(username))
                 .add(new NonNullInputValidator(password))
@@ -56,14 +64,24 @@ public class DefaultUserController implements UserController {
         Supplier<User> retrieveFunction = () -> userRepository.get(username);
 
         OutputValidatorChain<User> outputValidation = new OutputValidatorChain<>();
-        outputValidation.add(new PasswordOutputValidator(password));
+        outputValidation.add(new PasswordOutputValidator(encryption, password));
 
         return BusinessLogic.execute(retrieveFunction, inputValidation, outputValidation, new LoginExceptionHandler());
     }
 
     @Override
     public Either<ServerError, User> register(String username, String password) {
-        return null;
+        Supplier<User> saveAndRetrieveFunction = () -> {
+            byte[] data = encryption.encrypt(password.getBytes(StandardCharsets.UTF_8));
+            String base64 = Base64.getEncoder().encodeToString(data);
+
+            User user = new User(new LoginCredentials(username, base64));
+            userRepository.save(user);
+
+            return userRepository.get(username);
+        };
+
+        return BusinessLogic.execute(saveAndRetrieveFunction);
     }
 
     @Override
